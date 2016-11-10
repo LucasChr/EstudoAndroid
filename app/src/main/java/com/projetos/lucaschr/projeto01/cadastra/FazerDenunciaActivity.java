f86f8b0d5c36c402b7c3bbf4ff98c21a735f55a6
Lucas <lucaschr37@gmail.com>
2016-11-10 10:15:13 -0200

    Primeiro Commit

---
+++ app/src/main/java/com/projetos/lucaschr/projeto01/cadastra/FazerDenunciaActivity.java
@@ -0,0 +1,15 @@
+package com.projetos.lucaschr.projeto01.cadastra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.projetos.lucaschr.projeto01.R;
+
+public class FazerDenunciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fazer_denuncia);
+    }
+}
---
+++ app/src/main/java/com/projetos/lucaschr/projeto01/sqlite/BancoDados.java
@@ -0,0 +1,32 @@
+package com.projetos.lucaschr.projeto01.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by lucas on 04/11/16.
+ */
+public class BancoDados {
+
+    private static final String NOME_BANCO = "denuncia001";
+    private static final int VERSAO_BANCO = 1;
+
+    //Script
+    private static final String[] SCRIPT_DATABASE_DELETE = new String[] {"DROP TABLE IF EXISTS tbl_denuncia;"};
+
+    //Tabela com id sequencial usa-se _id
+    private static final String[] SCRIPT_DATABASE_CREATE = new String[]{
+            "create table tbl_denuncia(_id integer primary key, den_categoria text, den_descricao text, den_latitude text, den_longitude text, den_img text);"
+    };
+
+    private static SQLiteDatabase db;
+
+    public static SQLiteDatabase getDB(Context ctx) {
+        if (db == null) {
+            SQLiteHelper dbHelper = new SQLiteHelper(ctx, NOME_BANCO, VERSAO_BANCO, SCRIPT_DATABASE_CREATE, SCRIPT_DATABASE_DELETE);
+            db = dbHelper.getWritableDatabase();
+        }
+        return db;
+
+    }
+}
